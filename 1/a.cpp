fa4d495ede9c2abe7d7942365560cb93b1e6ea8f
Oran Looney <olooney@gmail.com>
2017-12-01 23:41:33 -0600

    alternative with more of a C++ flavor

--- 1/a.cpp
+++ 1/a.cpp
@@ -18,11 +18,32 @@ int f(const char* digits) {
     return sum;
 }
 
+int g(std::string digits) {
+    int sum = 0;
+
+    if ( digits.empty() ) return 0;
+
+    digits.push_back(digits[0]);
+    int prev = 0;
+    int current = digit(digits[0]);
+    auto second = ++digits.begin();
+
+    for ( auto iter = second; iter != digits.end(); iter++ ) {
+        prev = current;
+        current = digit(*iter);
+        if ( prev == current ) {
+            sum += prev;
+        }
+        
+    }
+    return sum;
+}
+
 int main(int argc, char** argv) {
     if ( argc != 2 ) {
         std::cout << "USAGE: a 1122" << std::endl;
     } else {
-        std::cout << f(argv[1])<< std::endl;
+        std::cout << g(argv[1])<< std::endl;
     }
     return 0;
 }
